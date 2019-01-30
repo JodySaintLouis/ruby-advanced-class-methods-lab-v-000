fe23c862cab3ae73b56f7f2fcc3134328e306098
Jamal Gelin <gelin.jamal@gmail.com>
2019-01-30 14:02:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,67 +1,74 @@
-
 class Song
-attr_accessor :name, :artist_name
- @@all = []
+  attr_accessor :name, :artist_name
+  @@all = []
 
 def self.all
-   @@all
+  @@all
 end
 
-def save
+def save	  
     self.class.all << self
-  end	  end
+  end
 
 def self.create
-  new_song = self.new
-  @@all << new_song
-  new_song
+  song = self.new
+  song.save
+  song
 end
 
- def self.new_by_name(song_name)
- new_song = self.new
- new_song.name = (song_name)
- new_song
+ def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
 end
 
- def self.create_by_name(song_name)
-  song_created = self.new_by_name(song_name)
-  @@all << song_created
-  song_created
+ def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  song.save
+  song
 end
 
- def self.find_by_name(song_name)
- @@all.find {|song| song.name == song_name}
+ def self.find_by_name(name)
+  self.all.detect do |song|
+    song.name == name
+  end
 end
 
- def self.find_or_create_by_name(song_name)
-self.find_by_name(song_name) || self.create_by_name(song_name)
+ def self.find_or_create_by_name(name)
+  dupe = Song.find_by_name(name)
+  if dupe == nil
+    Song.create_by_name(name)
+  else
+    dupe
+  end
 end
 
  def self.alphabetical
-   @@all.sort_by {|song| song.name}
+  self.all.sort_by do |song|
+    song.name
+  end
 end
 
  def self.new_from_filename(filename)
-  new_file = filename.split(" - ")
-  artist = new_file[0]
-  song = new_file[1].chomp(".mp3")
-  new_song = self.new
-  new_song.artist_name = artist
-  new_song.name = song
-  new_song
+  array = filename.split(/[^a-zA-Z\s]|\s-\s/)
+  song = self.new
+  song.name = array[1]
+  song.artist_name = array[0]
+  song
 end
 
  def self.create_from_filename(filename)
-  new_file = filename.split(" - ")
-  artist = new_file[0]
-  song = new_file[1].chomp(".mp3")
-  new_song = self.create
-  new_song.artist_name = artist
-  new_song.name = song
-  new_song
+  array = filename.split(/[^a-zA-Z\s]|\s-\s/)
+  song = self.new
+  song.name = array[1]
+  song.artist_name = array[0]
+  song.save
+  song
 end
 
  def self.destroy_all
-  @@all.clear
+  self.all.clear
 end
-end	end
+
+end
